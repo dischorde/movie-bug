bf078b7bdf842d99d9e7a0700b6605a27512bfee
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-03-31 16:15:13 -0700

    Set up most of auth for use later

---
+++ app/controllers/api/session_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(session_params[:username],
+                                     session_params[:password])
+    if @user
+      sign_in(@user)
+    else
+      render json: ["Invalid username or password"], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      sign_out
+      render json: {}
+    else
+      render json: ["No user to sign out"], status: 404
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
+end
---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,16 @@
+class Api::UserController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
