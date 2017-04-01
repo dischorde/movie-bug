b285dd3e88faf898c33cd531683894b20713c463
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-03-31 18:46:04 -0700

    Add router and app component, switch react-router to ^3.0.0

--- frontend/movie_bug.jsx
+++ frontend/movie_bug.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
+import Root from './components/root.jsx';
 import { fetchSearchResults } from './actions/result_actions.js';
 import { saveSearch } from './util/search_api_util.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    window.store = configureStore();
-    ReactDOM.render(<h1>I'm a root!</h1>, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={ store }/>, root);
     window.fetchSearchResults = fetchSearchResults;
     window.saveSearch = saveSearch;
 });
